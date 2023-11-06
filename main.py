c05611b6ce8fca994e8caf6fb95d66723a0728f2
carleyeshield <carlo_yamzon@yahoo.com>
2023-11-06 12:49:45 +0800

    Python Data Science

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+# word = 'cool'
+# list = [letter for letter in word[1:len(word)]]
+# join = ''.join(list)
+# first_letter = word[0]
+# new_word = join + first_letter + 'ay'
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# ph = ('Pig latin is cool')
+# ph = ('This is my string')
+# ph = ph.split()
+#
+# arr = []
+# for n in ph:
+#     arr.append(n.replace(n[0],'')+n[0]+'ay')
+# print(' '.join(arr))
 
+def pig_it(text):
+    ph = text.split()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    arr = []
+    for n in ph:
+        arr.append(n.replace(n[0],'') + n[0] + 'ay')
+    # print(' '.join(arr))
+    print(str(' '.join(arr)))
+    return arr
 
+pig_it('This is my string')
+pig_it('Pig latin is cool')
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
